2ba12e3bb2e3a20fedea4dd49605ed5ca2ed7c2a
Bipin Sahu <bipinsahu5@gmail.com>
2022-04-02 13:03:53 +0530

    Added useDispatch and useSelector hooks

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,21 +1,60 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 
-const SongDetail = (props) => {
-  const renderDetail = () => {
-    return props.song === null ? (
+//const SongDetail = (props) => {
+//   const renderDetail = () => {
+//     return props.selectedSong === null ? (
+//       <label>
+//         <h2 className="ui center aligned row ">Please Select a Song </h2>
+//       </label>
+//     ) : (<div>
+//       <img
+//         className="ui small bordered centered image"
+//         src={props.selectedSong.art}
+//       ></img>      
+//       <div className='ui meta'> Name: {props.selectedSong.name}</div> <br></br>
+//       <div className='ui meta'> Length: {props.selectedSong.length}</div></div>
+//     );
+//   };
+
+
+//   return (
+//     <React.Fragment>
+//       <h2>SongDetail</h2>
+//       <div className="ui placeholder segment">{renderDetail()}</div>
+//     </React.Fragment>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+// selectedSong: state.selectedSong,
+//   };
+// };
+
+// export default connect(mapStateToProps)(SongDetail);
+
+const SongDetail =()=>{
+
+  const selectedSong = useSelector(state=>state.selectedSong);
+
+    const renderDetail = () => {
+    return selectedSong === null ? (
       <label>
         <h2 className="ui center aligned row ">Please Select a Song </h2>
       </label>
     ) : (<div>
       <img
         className="ui small bordered centered image"
-        src={props.song.art}
+        src={selectedSong.art}
       ></img>      
-      <div className='ui meta'> Name: {props.song.name}</div> <br></br>
-      <div className='ui meta'> Length: {props.song.length}</div></div>
+      <div className='ui meta'> Name: {selectedSong.name}</div> <br></br>
+      <div className='ui meta'> Length: {selectedSong.length}</div></div>
     );
   };
+
+
+
 
   return (
     <React.Fragment>
@@ -25,10 +64,5 @@ const SongDetail = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    song: state.selectedSong,
-  };
-};
+export default SongDetail;
 
-export default connect(mapStateToProps)(SongDetail);
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,18 +1,65 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useDebugValue } from "react";
+import { connect,useDispatch,useSelector } from "react-redux";
 import { songSelect } from "../actions";
 
-class SongList extends React.Component {
-  renderList() {
-    return this.props.songs.map((song) => {
+// class SongList extends React.Component {
+//   renderList() {
+//     return this.props.songs.map((song) => {
+//       return (
+//         <div className="item" key={song.name}>
+//           <div className="ui right floated content">
+//             <button
+//               className="ui button"
+//               onClick={() => {
+//                   console.log(this.props)
+//                 this.props.songSelect(song);
+//               }}
+//             >
+//               Select
+//             </button>
+//           </div>
+//           <div className="middle aligned row">
+//             <h4>{song.name}</h4>
+//           </div>
+//         </div>
+//       );
+//     });
+//   }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h2>SongList</h2>
+//         <div className="ui placeholder segment">
+//           <div className="ui relaxed divided list">{this.renderList()}</div>
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   //console.log(state);
+//    return { songs: state.songs };
+ 
+// };
+// export default connect(mapStateToProps, { songSelect })(SongList);
+
+
+
+const SongList =()=>{
+
+  const songs = useSelector(state=>state.songs)
+  const dispatch = useDispatch();
+  //console.log(songs)
+   const  renderList=()=> {
+    return songs.map((song) => {
       return (
         <div className="item" key={song.name}>
           <div className="ui right floated content">
             <button
               className="ui button"
               onClick={() => {
-                  console.log(this.props)
-                this.props.songSelect(song);
+                 dispatch(songSelect(song));
               }}
             >
               Select
@@ -25,21 +72,21 @@ class SongList extends React.Component {
       );
     });
   }
-  render() {
+
+
+  
     return (
       <React.Fragment>
         <h2>SongList</h2>
         <div className="ui placeholder segment">
-          <div className="ui relaxed divided list">{this.renderList()}</div>
+          <div className="ui relaxed divided list">{renderList()}</div>
         </div>
       </React.Fragment>
     );
-  }
-}
+  
 
-const mapStateToProps = (state) => {
-  //console.log(state);
-   return { songs: state.songs };
- 
-};
-export default connect(mapStateToProps, { songSelect })(SongList);
+  
+
+
+}
+export default SongList;
